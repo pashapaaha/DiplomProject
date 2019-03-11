674a536a32672b0d49850bfb746588e079253942
Pavel Andreyev <paaha98@gmail.com>
2019-03-11 09:14:21 +0300

    Добавлена авторизация на экран авторизации и регистрации

--- app/src/main/java/com/example/paaha/findyourfriend/LoginActivity.kt
+++ app/src/main/java/com/example/paaha/findyourfriend/LoginActivity.kt
@@ -4,46 +4,111 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_login.*
-import com.google.firebase.auth.FirebaseAuth
-
 
 
 class LoginActivity : AppCompatActivity() {
 
     var mAuth: FirebaseAuth? = null
-
+    var currentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO: добавить автоматическую авторизацию
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         init()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        currentUser = mAuth?.currentUser
+        updateUI(currentUser != null)
     }
 
     private fun init() {
         mAuth = FirebaseAuth.getInstance()
 
         loginButton.setOnClickListener {
-
-            authorizedLayout.visibility = View.GONE
-            notAuthorizedLayout.visibility = View.VISIBLE
+            if (isValid()) {
+                logIn()
+            }
         }
 
-        registrationTextView.setOnClickListener{
+        registrationTextView.setOnClickListener {
             val intent = RegistrationActivity.newIntent(this)
             startActivity(intent)
         }
 
         logOutButton.setOnClickListener {
+            logOut()
+        }
+    }
+
+    private fun logIn() {
+        mAuth?.signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+            ?.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    updateUI(true)
+                } else {
+                    Toast.makeText(this, getString(R.string.failed_login), Toast.LENGTH_SHORT).show()
+                    try {
+                        if (it.exception != null) {
+                            throw it.exception!!
+                        }
+                    } catch (e: FirebaseAuthWeakPasswordException) {
+                        passwordEditText.error = e.message
+                        passwordEditText.requestFocus()
+                    } catch (e: FirebaseAuthUserCollisionException) {
+                        emailEditText.error = e.message
+                        emailEditText.requestFocus()
+                    } catch (e: FirebaseAuthInvalidCredentialsException) {
+                        emailEditText.error = e.message
+                        emailEditText.requestFocus()
+                    } catch (e: Exception) {
+                        Log.e(RegistrationActivity::class.java.name, e.message)
+                    }
+                }
+            }
+    }
+
+    private fun logOut() {
+        mAuth?.signOut()
+        updateUI(false)
+    }
+
+    private fun updateUI(isAuthorized: Boolean) {
+        if (isAuthorized) {
+
+            nameTextView.text = currentUser?.displayName ?: "null"
+            emailTextView.text = currentUser?.email ?: "null"
+
+            authorizedLayout.visibility = View.GONE
+            notAuthorizedLayout.visibility = View.VISIBLE
+        } else {
             authorizedLayout.visibility = View.VISIBLE
             notAuthorizedLayout.visibility = View.GONE
         }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val currentUser = mAuth?.currentUser
+    private fun isValid(): Boolean {
+        var result = true
+        if (!emailEditText.text.toString().matches("[a-zA-Z0-9]+(.[a-zA-Z0-9]*)*@[a-zA-Z0-9]*.[a-zA-Z]{2,}".toRegex())) {
+            emailEditText.error = getString(R.string.not_valid_email)
+            result = false
+        } else {
+            emailEditText.error = null
+        }
+        if (passwordEditText.text.toString().isEmpty()) {
+            passwordEditText.error = getString(R.string.input_password)
+            result = false
+        } else {
+            passwordEditText.error = null
+        }
+        return result
     }
 
     companion object {
--- app/src/main/java/com/example/paaha/findyourfriend/RegistrationActivity.kt
+++ app/src/main/java/com/example/paaha/findyourfriend/RegistrationActivity.kt
@@ -4,19 +4,98 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_registration.*
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+
 
 class RegistrationActivity : AppCompatActivity() {
 
     var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //TODO: реализовать перенос с экрана регистрации на экран с подтвержденным входом
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        init()
     }
 
-    fun init(){
+    fun init() {
         mAuth = FirebaseAuth.getInstance()
+
+        registrationButton.setOnClickListener {
+            if (isValid()) {
+                mAuth?.createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    ?.addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            val user = FirebaseAuth.getInstance().currentUser //TODO: добавление имени не работает
+                            val prifile = UserProfileChangeRequest.Builder()
+                                .setDisplayName(nameEditText.text.toString())
+                                .build()
+                        } else {
+                            Toast.makeText(this, getString(R.string.failed_reg), Toast.LENGTH_SHORT).show()
+                            try {
+                                if (it.exception != null) {
+                                    throw it.exception!!
+                                }
+                            } catch (e: FirebaseAuthWeakPasswordException) {
+                                passwordEditText.error = e.message
+                                passwordEditText.requestFocus()
+                            } catch (e: FirebaseAuthUserCollisionException) {
+                                emailEditText.error = e.message
+                                emailEditText.requestFocus()
+                            } catch (e: FirebaseAuthInvalidCredentialsException) {
+                                emailEditText.error = e.message
+                                emailEditText.requestFocus()
+                            } catch (e: Exception) {
+                                Log.e(RegistrationActivity::class.java.getName(), e.message)
+                            }
+
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun isValid(): Boolean {
+        var result = true
+
+        val name = nameEditText.text.toString()
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+        val confirmPassword = confirmPasswordEditText.text.toString()
+
+        if (name.isEmpty()) {
+            nameEditText.error = getString(R.string.input_name)
+            result = false
+        } else {
+            nameEditText.error = null
+        }
+        if (!email.matches("[a-zA-Z0-9]+(.[a-zA-Z0-9]*)*@[a-zA-Z0-9]*.[a-zA-Z]{2,}".toRegex())) {
+            emailEditText.error = getString(R.string.not_valid_email)
+            result = false
+        } else {
+            emailEditText.error = null
+        }
+        if (password.isEmpty()) {
+            passwordEditText.error = getString(R.string.input_password)
+            result = false
+        } else {
+            passwordEditText.error = null
+        }
+        if (confirmPassword != password) {
+            confirmPasswordEditText.error = getString(R.string.not_equals)
+            result = false
+        } else {
+            confirmPasswordEditText.error = null
+        }
+
+        return result
     }
 
     companion object {
